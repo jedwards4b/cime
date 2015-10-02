53405ef47c55fe877cc5463873e2530f03c7cf99
Jim Edwards <jedwards@ucar.edu>
2015-10-02 10:18:48 -0600

    work in progress

--- scripts/trythis.pl
+++ scripts/trythis.pl
@@ -1,19 +1,43 @@
 #!/user/bin/env perl
 BEGIN{
-my $cimeroot = $ENV{CIMEROOT};
-die "CIMEROOT environment variable not set" unless defined $cimeroot;
-die "CIMEROOT directory \"$cimeroot\" not found" unless (-d $cimeroot);
-unshift @INC, "$cimeroot/utils/perl5lib";
-require CIME::Base;
+    my $cimeroot = $ENV{CIMEROOT};
+    die "CIMEROOT environment variable not set" unless defined $cimeroot;
+    die "CIMEROOT directory \"$cimeroot\" not found" unless (-d $cimeroot);
+    unshift @INC, "$cimeroot/utils/perl5lib";
+    require CIME::Base;
 }
 use CIME::Base;
 
 my %opts;
 $opts{localopt}=7;
 $opts{loglevel}="WARN";
+print "HERE1 @ARGV<> $opts{loglevel}<> $opts{localopt} <> $opts{help}\n";
 CIME::Base->getopts(\%opts);
-
+print "HERE2 @ARGV<> $opts{loglevel}<> $opts{localopt} <> $opts{help}\n";
 GetOptions("localopt=s"=>$opts{localopt});
+print "HERE3 @ARGV<> $opts{loglevel}<> $opts{localopt} <> $opts{help}\n";
 
-print "HERE @ARGV $opts{loglevel}<> $opts{localopt}\n";
+__END__
+=head1 NAME
 
+create_newcase - CIME case generator script
+
+=head1 SYNOPSIS
+
+    create_newcase [options] 
+    Options:
+       -help             brief help message
+       -loglevel <level> set stdout message verbosity
+       -model <name>     Specifies target model system.
+
+=head1 OPTIONS
+=over 8
+=item B<-help>
+Prints a brief help message and exits.
+=item B<-loglevel>
+Can be DEBUG (most verbose), INFO (default), WARN, ERROR, FATAL(least verbose)    
+=back
+=head1 DESCRIPTION
+B<create_newcase> will read the given input file(s) and do something
+useful with the contents thereof.
+=cut
--- utils/perl5lib/CIME/Base.pm
+++ utils/perl5lib/CIME/Base.pm
@@ -1,8 +1,9 @@
 package CIME::Base;
-our $VERSION = "0.1";
+use 5.10.0;
 use strict;
 use warnings;
-use Getopt::Long qw(GetOptions :config pass_through);
+use Getopt::Long qw(GetOptions :config pass_through auto_help);
+use Pod::Usage;
 use base 'ToolSet';
 ToolSet->use_pragma('strict');
 ToolSet->use_pragma('warnings');
@@ -10,6 +11,7 @@ ToolSet->export('Log::Log4perl'=>undef,
 		'Getopt::Long'=>qw(GetOptions),
 );
 
+our $VERSION = "0.1";
 
 sub new {
      my $class = shift();
@@ -28,13 +30,43 @@ sub _init {
 
 
 sub getopts {
-    my ($self,$opts) = @_;
+    my ($self, $opts) = @_;
+    # Set default values here, 
+    # be careful not to overwrite defaults passed in
     $opts->{loglevel} = "INFO" unless(defined $opts->{loglevel});
-
-
-    GetOptions("loglevel=s" => $opts->{loglevel});
+    $opts->{help} = 0 unless(defined $opts->{help});
+    
+    # These are the options supported by all cime perl tools
+    GetOptions("loglevel=s" => \$opts->{loglevel},
+	       "h|help" => \$opts->{help},
+	) or pod2usage(2);
+    pod2usage(1) if($opts->{help});
+    
 }
-
-
-
 1;
+    
+__END__
+=head1 NAME
+
+CIME::Base - Base functionality for perl in CIME
+
+=head1 SYNOPSIS
+
+    sample [options] [file ...]
+     Options:
+       -help            brief help message
+       -loglevel        DEBUG,INFO,WARN,ERROR,FATAL
+=head1 OPTIONS
+=over 8
+=item B<-help>
+Print a brief help message and exits.
+=item B<-loglevel>
+    
+=back
+=head1 DESCRIPTION
+B<This program> will read the given input file(s) and do something
+useful with the contents thereof.
+=cut
+
+
+
