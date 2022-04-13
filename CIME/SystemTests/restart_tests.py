7768a7ab3fe67dfda5f542ac6f3daf01ad0243fc
Jim Edwards <jedwards@ucar.edu>
2022-04-13 09:43:15 -0600

    pylint

--- CIME/SystemTests/restart_tests.py
+++ CIME/SystemTests/restart_tests.py
@@ -5,6 +5,8 @@ Abstract class for restart tests
 
 from CIME.SystemTests.system_tests_compare_two import SystemTestsCompareTwo
 from CIME.XML.standard_module_setup import *
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
 
 logger = logging.getLogger(__name__)
 
@@ -36,6 +38,41 @@ class RestartTest(SystemTestsCompareTwo):
     def _case_two_setup(self):
         rest_n = self._case1.get_value("REST_N")
         stop_n = self._case1.get_value("STOP_N")
+        startdate = self._case1.get_value("RUN_STARTDATE")
+        starttod = self._case1.get_value("START_TOD")
+        restoption = self._case1.get_value("REST_OPTION")
+        sdate = datetime.fromisoformat(f"{startdate}") + timedelta(seconds=starttod)
+
+        if restoption.startswith("nhour"):
+            rpdate = sdate + timedelta(hours=rest_n)
+        elif restoption.startswith("nmonth"):
+            rpdate = sdate + relativedelta(months=rest_n)
+        elif restoption.startswith("nyear"):
+            rpdate = sdate + relativedelta(years=rest_n)
+        elif restoption.startswith("nday"):
+            rpdate = sdate + timedelta(days=rest_n)
+        elif restoption.startswith("nminute"):
+            rpdate = sdate + timedelta(minutes=rest_n)
+        elif restoption.startswith("nsecond"):
+            rpdate = sdate + timedelta(seconds=rest_n)
+        elif restoption.startswith("nstep"):
+            ncpl = 0
+            ncpl_base = self._case1.get_value("NCPL_BASE_PERIOD")
+            for compclass in self._case1.get_values("COMP_CLASSES"):
+                comp_ncpl = self._case1.get_value("{}_NCPL".format(compclass))
+                if comp_ncpl is not None:
+                    ncpl = max(ncpl, comp_ncpl)
+            if ncpl_base == "day":
+                rpdate = sdate + timedelta(seconds=(86400 / ncpl))
+            elif ncpl_base == "hour":
+                rpdate = sdate + timedelta(seconds=(3600 / ncpl))
+            elif ncpl_base == "year":
+                rpdate = sdate + relativedelta(years=(1.0 / ncpl))
+            elif ncpl_base == "decade":
+                rpdate = sdate + relativedelta(years=(10.0 / ncpl))
+        elif restoption == "date":
+            rpdate = datetime.fromisoformat(self._case1.get_value("REST_DATE"))
+
         stop_new = stop_n - rest_n
         expect(
             stop_new > 0,
@@ -48,3 +85,13 @@ class RestartTest(SystemTestsCompareTwo):
         self._case.set_value("STOP_N", stop_new)
         self._case.set_value("CONTINUE_RUN", True)
         self._case.set_value("REST_OPTION", "never")
+        self._case.set_value(
+            "RPOINTER_TIMESTAMP_DATE",
+            "{0:04d}-{1:02d}-{2:02d}".format(rpdate.year, rpdate.month, rpdate.day),
+        )
+        self._case.set_value(
+            "RPOINTER_TIMESTAMP_TOD",
+            "{0:05d}".format(
+                int(rpdate.hour * 3600 + rpdate.minute * 60 + rpdate.second)
+            ),
+        )
